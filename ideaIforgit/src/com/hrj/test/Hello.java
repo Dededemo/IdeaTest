b63c414f91330f737269969c82d2fcbab5cda59e
demo <1229610926@qq.com>
2023-03-03 18:18:09 +0800

    我对helo文件进行了第一次修改

--- ideaIforgit/src/com/hrj/test/Hello.java
+++ ideaIforgit/src/com/hrj/test/Hello.java
@@ -2,6 +2,8 @@ package com.hrj.test;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Wrold!");
+        System.out.println("Hello Wrold123!");
+        System.out.println("Hello Wrold1234!");
+        System.out.println("Hello Wrold1235!");
     }
 }
